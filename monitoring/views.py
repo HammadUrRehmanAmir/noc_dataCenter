a76ecc48b85ab8f14caa5a1ca5be10f747d8b0e6
HammadUrRehman <hammadurrehmanamir@gmail.com>
2025-02-24 16:56:51 +0500

    Add html pages

--- monitoring/views.py
+++ monitoring/views.py
@@ -6,3 +6,8 @@ from .models import NOC
 def noc_list(request):
     nocs = NOC.objects.all()
     return render(request, 'monitoring/noc_list.html', {'nocs', nocs})
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'monitoring/home.html')
--- noc_datacenter/urls.py
+++ noc_datacenter/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from monitoring.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('monitoring/', include('monitoring.urls')),
 ]
